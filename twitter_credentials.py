845b13d7efc7522fd2a99189640f4ddf49e6680e
Prasad Rao Jammuna <52504952+PrasadRaoJammuna@users.noreply.github.com>
2019-09-18 19:21:48 +0530

    changed keys

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,6 +1,6 @@
 
 
-ckey='CeFQZp0QvKg9W64IJeTcfn4Cs'
-csecret ='Uce6YZGPcTOxtwf9c0gmEpgKIogRNpnZ09f9XyrwdXcyQTaif1'
-atoken='1158407751904587781-y6KnexYlD5gHaNqI9j4GzhMJyC7Oyi'
-asecret ='cPlN3eqFxulfF6yhHkCp1KvxJlSJf80fOTBAEewH75zwf'
+ckey=''
+csecret =''
+atoken=''
+asecret =''
